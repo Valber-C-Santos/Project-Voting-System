44a410040b725b243f849fbd8291bb3a8d68bb11
Valber <valber_558@hotmail.com>
2024-04-05 15:18:35 -0300

    Requisito-03

--- src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
@@ -1,5 +1,26 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaEleitora{
+/**
+ * class PessoaEleitora extends Pessoa.
+ */
 
+public class PessoaEleitora extends Pessoa {
+  private String cpf;
+
+  /**
+  * class PessoaEleitora extends Pessoa.
+  */
+
+  public PessoaEleitora(String nome, String cpf) {
+    this.nome = nome;
+    this.cpf = cpf;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
 }
