4258618dc06ae01a304a09e6d798abb9755ddcf6
Valber <valber_558@hotmail.com>
2024-04-05 14:22:31 -0300

    Requisito-01

--- src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
+++ src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
@@ -1,5 +1,14 @@
 package com.betrybe.sistemadevotacao;
 
 public abstract class Pessoa {
-  
+
+  protected String nome;
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
 }
